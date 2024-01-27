db8f5e563fa4d927ae29d5999a9acb5252cf6275
sujaldangal08 <sujaldangal69@gmail.com>
2024-01-27 21:12:01 +0545

    commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pickle
 
 app = Flask(__name__, template_folder="template")
 model = pickle.load(open("./models/cat.pkl", "rb"))
-print("Model Loaded")
+print("Model Loaded")    
 
 @app.route("/",methods=['GET'])
 @cross_origin()
@@ -31,46 +31,12 @@ def predict():
 		maxTemp = float(request.form['maxtemp'])
 		# Rainfall
 		rainfall = float(request.form['rainfall'])
-		# Evaporation
-		evaporation = float(request.form['evaporation'])
-		# Sunshine
-		sunshine = float(request.form['sunshine'])
-		# Wind Gust Speed
-		windGustSpeed = float(request.form['windgustspeed'])
-		# Wind Speed 9am
-		windSpeed9am = float(request.form['windspeed9am'])
-		# Wind Speed 3pm
-		windSpeed3pm = float(request.form['windspeed3pm'])
-		# Humidity 9am
-		humidity9am = float(request.form['humidity9am'])
-		# Humidity 3pm
-		humidity3pm = float(request.form['humidity3pm'])
-		# Pressure 9am
-		pressure9am = float(request.form['pressure9am'])
-		# Pressure 3pm
-		pressure3pm = float(request.form['pressure3pm'])
-		# Temperature 9am
-		temp9am = float(request.form['temp9am'])
-		# Temperature 3pm
-		temp3pm = float(request.form['temp3pm'])
-		# Cloud 9am
-		cloud9am = float(request.form['cloud9am'])
-		# Cloud 3pm
-		cloud3pm = float(request.form['cloud3pm'])
-		# Cloud 3pm
+		
 		location = float(request.form['location'])
-		# Wind Dir 9am
-		winddDir9am = float(request.form['winddir9am'])
-		# Wind Dir 3pm
-		winddDir3pm = float(request.form['winddir3pm'])
-		# Wind Gust Dir
-		windGustDir = float(request.form['windgustdir'])
-		# Rain Today
+		
 		rainToday = float(request.form['raintoday'])
 
-		input_lst = [location , minTemp , maxTemp , rainfall , evaporation , sunshine ,
-					 windGustDir , windGustSpeed , winddDir9am , winddDir3pm , windSpeed9am , windSpeed3pm ,
-					 humidity9am , humidity3pm , pressure9am , pressure3pm , cloud9am , cloud3pm , temp9am , temp3pm ,
+		input_lst = [location , minTemp , maxTemp,
 					 rainToday , month , day]
 		pred = model.predict(input_lst)
 		output = pred
